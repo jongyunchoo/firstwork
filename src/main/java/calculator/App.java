ee2a3b8352243b07ecd4f3858d8075dd5c7f1e03
Robbie <robbie331@>
2024-07-24 23:30:48 +0900

    level 1 - 3

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -9,15 +9,34 @@ public class App {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("첫 번째 숫자를 입력하세요: ");
-        int a1 = sc.nextInt();
+        int num1 = sc.nextInt();
         System.out.println("두 번째 숫자를 입력하세요: ");
-        int b1 = sc.nextInt();
+        int num2 = sc.nextInt();
 
         System.out.println("사칙연산 기호를 입력하세요: "); // +, -, *, /
         char operator = sc.next().charAt(0);
 
-        System.out.println("operator: " + operator);
+        int result = 0;
 
-        //d
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if(num2 == 0){
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다. ");
+                } else{
+                    result = num1 / num2;
+                } break;
+            default:
+        }     System.out.println("결과 : " + result);
+
     }
+
 }
